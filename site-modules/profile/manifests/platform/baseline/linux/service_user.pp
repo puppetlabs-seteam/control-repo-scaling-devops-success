48af22a36b7117ce1f42fb123e2ad8ff9f6f5de3
Thomas Linkin <trlinkin@gmail.com>
2019-06-04 13:45:36 -0400

    Correct values for the local admin users

--- site-modules/profile/manifests/platform/baseline/linux/service_user.pp
+++ site-modules/profile/manifests/platform/baseline/linux/service_user.pp
@@ -2,13 +2,13 @@ class profile::platform::baseline::linux::service_user{
 
   group { 'admins_local':
     ensure => present,
-    gid    => 10001,
+    gid    => 10000,
   }
 
   user{ 'tom_local':
     ensure   => present,
     uid      => 10001,
-    gid      => 10001,
+    gid      => 10000,
     password => '$6$KzLPJe2tmDUcqwah$tOWJuDcgir1vi4M9btVkg/NjJpVBbxs9Q7sZ97wBC3Z5lM4MN4sjFOnxfafJ93NhLgNsINHao0SGaLJhkVasg0',
     groups   => ['wheel'],
   }
@@ -16,7 +16,7 @@ class profile::platform::baseline::linux::service_user{
   user{ 'abir_local':
     ensure   => present,
     uid      => 10002,
-    gid      => 10002,
+    gid      => 10000,
     password => '6$de.E79ZLrAyCtS1I$XydX5VUsCIJiPrsOfh2U9ceW1E56aNDJQnfhGjDG2L54J4AIgqqBtVyG2AteCIYcGBi7Y9PvUpbzO87MeJrvk1',
     groups   => ['wheel'],
   }
