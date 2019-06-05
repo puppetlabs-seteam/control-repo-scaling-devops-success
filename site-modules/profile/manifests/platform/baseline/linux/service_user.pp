b37826591cca2e781d85607483dc5d10df440a5d
Thomas Linkin <trlinkin@gmail.com>
2019-06-05 12:01:19 -0400

    Update password for local admin use: tom_local

--- site-modules/profile/manifests/platform/baseline/linux/service_user.pp
+++ site-modules/profile/manifests/platform/baseline/linux/service_user.pp
@@ -9,7 +9,7 @@ class profile::platform::baseline::linux::service_user{
     ensure   => present,
     uid      => 10001,
     gid      => 10000,
-    password => '$6$KzLPJe2tmDUcqwah$tOWJuDcgir1vi4M9btVkg/NjJpVBbxs9Q7sZ97wBC3Z5lM4MN4sjFOnxfafJ93NhLgNsINHao0SGaLJhkVasg0',
+    password => '$6$f7ad3f147661c04e$LuY2G7Ka78bBUnpTWOlkfN5ILIEupFGiRQDi..9QV0nDgkRyagcxAtVSXWOOE6Fwe2dK3tAPlYJ.Q.uvfQmGq1',
     groups   => ['wheel'],
   }
 
