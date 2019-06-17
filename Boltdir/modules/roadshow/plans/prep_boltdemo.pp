3c6db2bf78598e69f783fa27a43110d5667352f5
Paul Reed <paul.reed@puppet.com>
2019-06-17 14:09:34 -0400

    Updates to bolt demo prep

--- Boltdir/modules/roadshow/plans/prep_boltdemo.pp
+++ Boltdir/modules/roadshow/plans/prep_boltdemo.pp
@@ -3,6 +3,7 @@ plan roadshow::prep_boltdemo(
   String[1] $branch_name,
   String[1] $demo_host_id = '0',
   String[1] $demo_user = 'puppetinstructor',
+  String[1] $demo_pass = '@Pupp3t1abs',
   String[1] $source_inventory = 'roadshow/inventory.yaml',
   String[1] $source_student_pem = 'roadshow/student.pem',
   String[1] $source_puppetfile = 'roadshow/Puppetfile',
@@ -25,8 +26,6 @@ plan roadshow::prep_boltdemo(
   run_command("mkdir -Force -p ${demo_taskdir}", $demo_host, "Creating Boltdir for ${demo_user} account on ${demo_host}")
 
   # Upload inventory.yaml, student.pem, Puppetfile and install_splunk_fowarder paln to the demo host Boltdir
-  #upload_file($source_inventory, $demo_boltdir, $demo_host, "Uploading inventory.yaml to ${demo_host}")
-  #upload_file($source_student_pem, $demo_boltdir, $demo_host, "Uploading student.pem to ${demo_host}")
   run_script('roadshow/retrieve_branch_info.ps1', $demo_host, 'arguments' => ['MaxHosts',$max_hosts,'Branch',$branch_name,'SavePath',$demo_userdir], "Running inventory prep script on ${demo_host}")
   upload_file($source_puppetfile, $demo_boltdir, $demo_host, "Uploading Puppetfile to ${demo_host}")
   upload_file($source_psscript, $demo_boltdir, $demo_host, "Uploading ${source_psscript} to ${demo_host} into ${demo_boltdir}")
@@ -37,4 +36,10 @@ plan roadshow::prep_boltdemo(
 
   # Install Puppetfile on demo host
   run_command('bolt puppetfile install', $demo_host, "Installing Puppetfile for ${demo_user} on ${demo_host}")
+
+  #Activate Firewalls on Windows nodes
+  run_command('bolt command run \'Set-Service "MpsSvc" -StartupType Automatic\' -n allwindows', $demo_host, "Enabling Firewall on Windows Hosts")
+  run_command('bolt command run \'netsh advfirewall firewall add rule name="Open Port 5985" dir=in action=allow protocol=TCP localport=5985\' -n allwindows', $demo_host, "Openning Firewall Port 5985 on Windows Hosts")
+  run_command('bolt command run \'netsh advfirewall firewall add rule name="Open Port 5986" dir=in action=allow protocol=TCP localport=5986\' -n allwindows', $demo_host, "Openning Firewall Port 5986 on Windows Hosts")
+  run_command('bolt command run \'Set-Service -Name "MpsSvc" -Status Running\' -n allwindows', $demo_host, "Starting Firewall Service on Windows Hosts")
 }
