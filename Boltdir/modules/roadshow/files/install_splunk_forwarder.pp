c3479a4d821d559890bfa067efbc99c3182a4b3a
Paul Reed <paul.reed@puppet.com>
2019-07-22 18:45:14 -0400

    Update to fix splunk forwarder installation on Windows with recent Splunk module update.

--- Boltdir/modules/roadshow/files/install_splunk_forwarder.pp
+++ Boltdir/modules/roadshow/files/install_splunk_forwarder.pp
@@ -1,7 +1,7 @@
 # Bolt plan to install a splunk forwarder
 plan tools::install_splunk_forwarder(
   TargetSpec $nodes,
-  String[1] $splunk_server='',
+  String[1] $splunk_server='splunk.example.com',
 ){
   apply_prep($nodes)
 
@@ -14,18 +14,8 @@ plan tools::install_splunk_forwarder(
       class { '::splunk::forwarder':
         package_provider => 'chocolatey',
         package_name     => 'splunk-universalforwarder',
-#        install_options  => [
-#          'INSTALLDIR=C:\\Program Files\\SplunkUniversalForwarder',
-#          'AGREETOLICENSE=YES',
-#          'LAUNCHSPLUNK=0',
-#          'SERVICESTARTTYPE=auto',
-#          'WINEVENTLOG_APP_ENABLE=1',
-#          'WINEVENTLOG_SEC_ENABLE=1',
-#          'WINEVENTLOG_SYS_ENABLE=1',
-#          'WINEVENTLOG_FWD_ENABLE=1',
-#          'WINEVENTLOG_SET_ENABLE=1',
-#          'ENABLEADMON=1',
-#        ]
+        secret_file      => "C:\\ProgramData\\splunk_secrets",
+        install_options  => [ ],
       }
     }
     else {
